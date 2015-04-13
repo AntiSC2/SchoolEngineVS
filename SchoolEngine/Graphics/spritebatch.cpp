a22b97b4a254e6c99018eebadebcb675cc6b327c
AntiSC2 <jakoba1.durador312@gmail.com>
2015-04-13 18:22:31 +0200

    Fixed SpriteBatch bug
    Fixed a bug where the sprite batch would pass 3 values for the position
    to the shader instead of 2. This caused sprites not to be rendered.

--- SchoolEngine/Graphics/spritebatch.cpp
+++ SchoolEngine/Graphics/spritebatch.cpp
@@ -27,7 +27,7 @@ void SpriteBatch::init() {
    glEnableVertexAttribArray(1);
    glEnableVertexAttribArray(2);
 
-   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
    glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, color));
    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, UV));
 
